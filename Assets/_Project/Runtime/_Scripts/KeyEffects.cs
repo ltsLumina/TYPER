a396ffe9b9a5e62a25488f79f8f89f9b8186da1b
Lum <itsluminas@gmail.com>
2025-09-08 17:23:54 +0200

    git failed

--- Assets/_Project/Runtime/_Scripts/KeyEffects.cs
+++ Assets/_Project/Runtime/_Scripts/KeyEffects.cs
@@ -1,8 +1,10 @@
+#region
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+#endregion
 
 public partial class KeyController
 {
@@ -14,7 +16,7 @@ public partial class KeyController
 		Right,
 		All
 	}
-	
+
 	/// <param name="direction"> Direction to look for an adjacent key. </param>
 	/// <param name="adjacentKeys">
 	///     If direction is All, this will be populated with all adjacent keys found. Otherwise, it
@@ -38,32 +40,32 @@ public partial class KeyController
 		{
 			case Direction.Up:
 				adjacentKeys = null;
-				return row > 0 ? keys[row - 1][Mathf.Min(col, keys[row - 1].Count - 1)] : null;
+				return row > 0 ? Keys[row - 1][Mathf.Min(col, Keys[row - 1].Count - 1)] : null;
 
 			case Direction.Down:
 				adjacentKeys = null;
-				return row < keys.Count - 1 ? keys[row + 1][Mathf.Min(col, keys[row + 1].Count - 1)] : null;
+				return row < Keys.Count - 1 ? Keys[row + 1][Mathf.Min(col, Keys[row + 1].Count - 1)] : null;
 
 			case Direction.Left:
 				adjacentKeys = null;
-				return col > 0 ? keys[row][col - 1] : null;
+				return col > 0 ? Keys[row][col - 1] : null;
 
 			case Direction.Right:
 				adjacentKeys = null;
-				return col < keys[row].Count - 1 ? keys[row][col + 1] : null;
+				return col < Keys[row].Count - 1 ? Keys[row][col + 1] : null;
 
 			case Direction.All: // return the first adjacent key found in every direction
 				var directions = new[] { Direction.Up, Direction.Down, Direction.Left, Direction.Right };
 				adjacentKeys = directions.Select(dir => GetAdjacentKey(keycode, dir, out _)).Where(adjacent => adjacent != null).ToList();
-				
+
 				// return self to indicate multiple keys found
-				return keys[row][col];
+				return Keys[row][col];
 
 			default:
 				throw new ArgumentOutOfRangeException(nameof(direction));
 		}
 	}
-	
+
 	/// <param name="includeSelf"> Whether to include the specified key in the returned list. </param>
 	/// <returns> Returns a list of all keys surrounding the specified key (up to 8 keys). </returns>
 	public List<Key> GetSurroundingKeys(KeyCode keycode, bool includeSelf = false)
@@ -77,11 +79,11 @@ public partial class KeyController
 		{
 			for (int c = col - 1; c <= col + 1; c++)
 			{
-				if (r >= 0 && r < keys.Count && c >= 0 && c < keys[r].Count && (r != row || c != col)) { surroundingKeys.Add(keys[r][c]); }
+				if (r >= 0 && r < Keys.Count && c >= 0 && c < Keys[r].Count && (r != row || c != col)) surroundingKeys.Add(Keys[r][c]);
 			}
 		}
 
-		if (includeSelf) surroundingKeys.Add(keys[row][col]);
+		if (includeSelf) surroundingKeys.Add(Keys[row][col]);
 
 		return surroundingKeys;
 	}
@@ -90,29 +92,26 @@ public partial class KeyController
 	public List<List<Key>> GetWaveKeys()
 	{
 		List<List<Key>> wave = new ();
-		int maxCols = keys.Max(row => row.Count);
+		int maxCols = Keys.Max(row => row.Count);
 
 		for (int col = 0; col < maxCols; col++)
 		{
-			List<Key> waveRow = (from t in keys where col < t.Count select t[col]).ToList();
+			List<Key> waveRow = (from t in Keys where col < t.Count select t[col]).ToList();
 			wave.Add(waveRow);
 		}
 
 		return wave;
 	}
-	
+
 	IEnumerator PerformWaveEffect(List<List<Key>> wave, float delayBetweenColumns)
 	{
 		foreach (List<Key> column in wave)
 		{
-			foreach (var key in column)
-			{
-				key.Activate(true, 2.5f, key);
-			}
+			foreach (Key key in column) key.Activate(true, 2.5f, key);
 			yield return new WaitForSeconds(delayBetweenColumns);
 		}
 	}
-	
+
 	public void Wave(float delayBetweenColumns = 0.1f)
 	{
 		var wave = GetWaveKeys();
